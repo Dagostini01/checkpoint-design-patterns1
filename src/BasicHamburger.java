63b50b0217cf5ba3c9fca4374125a0fd6e0bd2c2
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-08-30 21:06:41 -0300

    classe hamburger basic concluida

---
+++ src/BasicHamburger.java
@@ -0,0 +1,11 @@
+public class BasicHamburger implements Hamburger {
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
